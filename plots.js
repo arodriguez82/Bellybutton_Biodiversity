d396f57aee6176a7592fa18a594ecd8a3f61d25e
Anita Rodriguez <arodriguez82@gmail.com>
2020-03-01 18:04:48 -0800

    Updating Files

--- plots.js
+++ plots.js
@@ -1,37 +1,113 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
-
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
-
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
+  function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
   }
-};
 
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+  function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+      var PANEL = d3.select("#sample-metadata");
+  
+      PANEL.html("");
+      PANEL.append("h6").text("ID: " + result.id);
+      PANEL.append("h6").text("Ethnicity: " + result.ethnicity);
+      PANEL.append("h6").text("Gender: " + result.gender);
+      PANEL.append("h6").text("Age: " + result.age);
+      PANEL.append("h6").text("Location: " + result.location);
+      PANEL.append("h6").text("BBType: " + result.bbtype);
+      PANEL.append("h6").text("Wash Freq: " + result.wfreq);
+    });
+  };
+
+//Function to build the charts
+function buildCharts(sample){
+    d3.json("samples.json").then((data) => {
+        var OTU = data.samples;
+        var resultArray = OTU.filter(sampleObj => sampleObj.id == sample);
+        var otuValues = resultArray[0];
+        var otuIds = otuValues.otu_ids;
+        var otuLabels = otuValues.otu_labels;
+        var sampleValues = otuValues.sample_values;
+
+        // Wash Freq Variable for gauge chart
+        var metadata = data.metadata;
+        var resultData = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultData[0];
+        var wFreq = result.wfreq;
+
+// Bar Chart
+   
+    var trace = {
+        x: sampleValues.slice(0,11).reverse(),
+        y: "OTU " + otuIds.slice(0,11).reverse(),
+        type: "bar",
+        orientation: 'h',
+        
+     };
+    var data = [trace];
+    var layout = {
+        title: "Top Bacterial Species found in ID " + otuValues.id,
+        xaxis: { title: "Sample Values"},
+        yaxis: { title: "OTU #"},
+        margin: {t: 30, l: 100},
+    }
+    Plotly.newPlot("bar", data, layout);
+
+// Bubble Chart
+
+    var trace1 ={
+      x: otuIds,
+      y: sampleValues,
+      mode: 'markers',
+      marker: {
+          size: sampleValues,
+          sizemode: 'area',
+          sizeref: .25,
+          color: otuIds,
+      }
+  };
+
+  var data = [trace1];
+  var layout = {
+      title: 'Amount of Bacteria by OTU ID #',
+      xaxis: {title:{text: 'OTU ID #',}},
+      showlegend: false 
+  };
+  Plotly.newPlot('bubble', data, layout); 
+
+// Gauge Chart
+
+  var data = [
+    {
+      domain: { x: [resultArray, wFreq], y: [resultArray, wFreq] },
+      value: wFreq,
+      title: { text: "Belly Button Washing Frequency<br>Scrubs per Week" },
+      type: "indicator",
+      gauge: {axis: {range: [null, 9]}},
+      mode: "gauge+number"
+    }
+  ];
+  
+  var layout = { width: 600, height: 500, margin: { t: 0, b: 0 } };
+  Plotly.newPlot('gauge', data, layout);
+
+  });
+};
